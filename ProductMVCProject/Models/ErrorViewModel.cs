cb689ee8496f5e9980d07182fc4782e15d27d512
Yash Chhatrola <chhatrolayash@gmail.com>
2026-01-12 11:19:28 +0530

    Add project files.

---
+++ ProductMVCProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProductMVCProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
