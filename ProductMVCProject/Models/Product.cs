07007e1776d3a00c6c508472c8099a4a476fbe65
Yash Chhatrola <chhatrolayash@gmail.com>
2026-01-12 12:15:45 +0530

    new file created

---
+++ ProductMVCProject/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductMVCProject.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        [Range(0,10, ErrorMessage = "Value Must not be Greater then 10")]
+        public int Stock { get; set; }
+
+    }
+}
--- ProductMVCProject/Program.cs
+++ ProductMVCProject/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using ProductMVCProject.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(Option => Option.UseSqlServer(builder.Configuration.GetConnectionString("ProductConnection")));
 
 var app = builder.Build();
 
